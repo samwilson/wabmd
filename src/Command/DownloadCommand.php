5fddc951227dc066508e8357cc1b13ff5d0cbc7b
Sam Wilson <sam@samwilson.id.au>
2024-03-02 15:51:34 +0800

    Switch to Symfony

---
+++ src/Command/DownloadCommand.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[AsCommand(
+	name: 'app:download',
+	description: 'Download records from the WA BDM index into JSON and CSV files in the data/ directory.',
+)]
+class DownloadCommand extends CommandBase {
+
+	private HttpClientInterface $httpClient;
+
+	private SymfonyStyle $io;
+
+	public function __construct( HttpClientInterface $httpClient ) {
+		parent::__construct();
+		$this->httpClient = $httpClient;
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
+		parent::execute( $input, $output );
+		$this->io = new SymfonyStyle( $input, $output );
+
+		// $types = [
+		// 	'births' => [
+		// 		'plural' => 'births',
+		// 		'singular' => 'birth',
+		// 		'fields' => [
+		// 			'surname',
+		// 			'givenNames',
+		// 			'gender',
+		// 			'father',
+		// 			'mother',
+		// 			'birthPlace',
+		// 			'registrationDistrict',
+		// 			'registrationNumber',
+		// 			'registrationYear',
+		// 		],
+		// 	],
+		// 	'deaths' => [
+		// 		'plural' => 'deaths',
+		// 		'singular' => 'death',
+		// 		'fields' => [
+		// 			'surname',
+		// 			'givenNames',
+		// 			'gender',
+		// 			'father',
+		// 			'mother',
+		// 			'deathPlace',
+		// 			'yearOfDeath',
+		// 			'age',
+		// 			'registrationDistrict',
+		// 			'registrationNumber',
+		// 			'registrationYear',
+		// 		],
+		// 	],
+		// 	'marriages' => [
+		// 		'plural' => 'marriages',
+		// 		'singular' => 'marriage',
+		// 		'fields' => [
+		// 			'surname',
+		// 			'givenNames',
+		// 			'gender01',
+		// 			'gender02',
+		// 			'spouseSurname',
+		// 			'spouseGivenNames',
+		// 			'marriagePlace',
+		// 			'registrationYear',
+		// 			'registrationNumber',
+		// 			'registrationDistrict',
+		// 			'yearOfMarriage',
+		// 		],
+		// 	],
+		// ];
+		foreach ( $this->types as $info ) {
+			$this->getTypeData( $info['plural'], $info['singular'], array_keys( $info['fields'] ) );
+		}
+		return Command::SUCCESS;
+	}
+
+	private function getTypeData( $typePlural, $typeSingular, $fieldNames ) {
+		$dataDir = $this->getDataDir();
+		$jsonFilename = $dataDir . "/$typePlural.json";
+		$csvFilename = $dataDir . "/$typePlural.csv";
+
+		if ( !file_exists( $jsonFilename ) ) {
+			$this->io->writeln( "Downloading $typePlural JSON data" );
+			$url = "https://justice.wa.gov.au/_apps/DoJWebsite/onlineIndex/" . $typeSingular . "Records";
+			$response = $this->httpClient->request( 'GET', $url );
+			file_put_contents( $jsonFilename, $response->getContent() );
+		}
+
+		// Get the JSON data.
+		$json = file_get_contents( $jsonFilename );
+		$data = json_decode( $json, true );
+		$this->io->writeln(
+			'Writing ' . number_format( count( $data ) ) . ' ' . $typeSingular . ' rows to ' . $csvFilename
+		);
+
+		$dataNew = [];
+		foreach ( $data as $datum ) {
+			// Get only the required fields, in the order we want them.
+			$datumFiltered = [];
+			foreach ( $fieldNames as $field ) {
+				$datumFiltered[] = $datum[$field];
+			}
+			// Hacky way to get a single CSV line.
+			$f = fopen( 'php://memory', 'r+' );
+			fputcsv( $f, $datumFiltered );
+			rewind( $f );
+			$csvLine = stream_get_contents( $f );
+			$dataNew[] = $csvLine;
+		}
+
+		// Sort and output.
+		sort( $dataNew );
+		file_put_contents( $csvFilename, implode( ',', $fieldNames ) . "\n" . implode( '', $dataNew ) );
+	}
+
+}
---
+++ src/Command/ImportCommand.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Command;
+
+use App\Database;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+	name: 'app:import',
+	description: 'Import the downloaded data into the local database, from the data/ directory.',
+)]
+class ImportCommand extends CommandBase {
+
+	private $db;
+
+	public function __construct( Database $db ) {
+		parent::__construct();
+		$this->db = $db;
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
+		$io = new SymfonyStyle( $input, $output );
+		$progressBar = $io->createProgressBar();
+		foreach ( $this->types as $info ) {
+			$io->writeln( 'Importing ' . $info['plural'] );
+			$progressBar->start();
+			$file = fopen( dirname( __DIR__, 2 ) . '/data/' . $info['plural'] . '.csv', 'r' );
+			$rowNum = 0;
+			$dataChunk = [];
+			$row = true;
+			while ( $row !== false ) {
+				$row = fgetcsv( $file );
+				$rowNum++;
+				if ( $rowNum === 1 ) {
+					continue;
+				}
+				if ( count( $dataChunk ) <= 100 ) {
+					$rowData = array_combine( $info['fields'], $row );
+					$rowData['registration_district_id'] = $this->db->getDistrictId(
+						$rowData['registration_district_id']
+					);
+					$placeColName = $info['singular'] . '_place_id';
+					$rowData[$placeColName] = $this->db->getPlaceId( $rowData[$placeColName] );
+					$dataChunk[] = $rowData;
+					$progressBar->advance();
+				} else {
+					$this->db->saveMultiple( $info['plural'], $dataChunk );
+					$dataChunk = [];
+				}
+			}
+			$io->newLine();
+		}
+		return Command::SUCCESS;
+	}
+
+	private function saveChunck( $data ) {
+		$rows = [];
+		foreach ( $data as $datum ) {
+			$districtId = $this->db->getDistrictId( $datum['registrationDistrict'] );
+			$birthPlaceId = $this->db->getPlaceId( $datum['birthPlace'] );
+			$birthRecords[] = [
+				'surname' => $datum['surname'],
+				'given_names' => $datum['givenNames'],
+				'gender' => $datum['gender'],
+				'father' => $datum['father'],
+				'mother' => $datum['mother'],
+				'birth_place_id' => $birthPlaceId,
+				'year_of_birth' => $datum['yearOfBirth'],
+				'year_of_birth_from' => $datum['yearOfBirthFrom'],
+				'year_of_birth_to' => $datum['yearOfBirthTo'],
+
+				'registration_district_id' => $districtId,
+				'registration_year' => $datum['registrationYear'],
+				'registration_number' => $datum['registrationNumber'],
+
+				'record_id' => $datum['recordID'],
+				'record_type_id' => $datum['recordTypeID'],
+			];
+		}
+		$this->db->saveMultiple( $rows );
+	}
+}
