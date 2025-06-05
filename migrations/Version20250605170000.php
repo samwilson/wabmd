04adca50a8846d6d19862ca10b8f01568dfb0d52
Sam Wilson <sam@samwilson.id.au>
2025-06-05 17:41:41 +0800

    Add missing DB indices for querying WikiTree progress
    Also update PHP dependencies, and remove the unused Maker bundle.

---
+++ migrations/Version20250605170000.php
@@ -0,0 +1,20 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250605170000 extends AbstractMigration {
+
+	public function getDescription(): string {
+		return 'Add indices for counting WikiTree progress by year.';
+	}
+
+	public function up( Schema $schema ): void {
+		$this->addSql( 'ALTER TABLE births ADD INDEX idx_yearofbirth_wikitree (year_of_birth, wikitree)' );
+		$this->addSql( 'ALTER TABLE deaths ADD INDEX idx_yearofdeath_wikitree (year_of_death, wikitree)' );
+	}
+}
